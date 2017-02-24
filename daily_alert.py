b746b90541bbd711bfa6c7e8eca97fec32a31a2f
Simon Arsenault <simarsenault@gmail.com>
2017-02-24 10:55:40 -0500

    Send notifications by email and/or sms (twilio)

--- daily_alert.py
+++ daily_alert.py
@@ -1,16 +1,26 @@
 import json
 from videotron import Videotron
 from email_sender import Email_Sender
+from twilio_sms import Twilio_SMS_Sender
 
 
 if __name__ == '__main__':
     with open('config.json') as config_file:
         config = json.load(config_file)
 
-    videotron = Videotron(config['videotron_account'])
-    mailer = Email_Sender(config['smtp'])
+    config_email = config['email']
+    config_twilio_sms = config['twilio_sms']
 
+    videotron = Videotron(config['videotron_account'])
     current_month_usage = videotron.get_current_month_usage()
 
-    message = 'Usage: {usage} {unit}<br/>Maximum: {maximum} {unit}<br/>Day(s) remaining: {days_remaining}'.format(**current_month_usage)
-    mailer.send(config['email'], message)
+    message = 'Usage: {usage} {unit}\nMaximum: {maximum} {unit}\nDay(s) remaining: {days_remaining}'.format(**current_month_usage)
+
+    if config_email['enabled']:
+        mailer = Email_Sender(config['smtp'])
+        mailer.send(config_email, message)
+
+    if config_twilio_sms['enabled']:
+        sms = Twilio_SMS_Sender(config_twilio_sms['account']['sid'], config_twilio_sms['account']['auth_token'])
+        for number in config_twilio_sms['sms']["to_numbers"]:
+            sms.send_sms(config_twilio_sms['sms']["from_number"], number, message)
