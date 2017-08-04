f0092ac30c8f463f7129322e295af63acc872a9c
Simon Arsenault <simarsenault@gmail.com>
2017-08-04 12:04:03 -0400

    Use official API

--- videotron.py
+++ videotron.py
@@ -1,30 +1,23 @@
 import requests
-import re
+import json
 import datetime
 
 
 class Videotron:
-    PATTERN_INPUT_HIDDEN = re.compile(r"<input type=\"hidden\" name=\"(?P<tokenName>\w+)\" value=\"(?P<tokenValue>[\w.]+)\">")
-    PATTERN_MONTHLY_INTERNET_USAGE = re.compile(r"<span class=\"quantities\">(?P<current>[\d.]+) / (?P<maximum>[\d.]+).*(?P<unit>GB|MB)</span>")
-    PATTERN_DAYS_IN_MONTH_REMAINING = re.compile(r"<p class=\"note_reset\">Number of days before reverting to zero: (?P<daysLeft>\d+) days?</p>")
-    PATTERN_LAST_UPDATE = re.compile(r"<p class=\"details_mise_a_jour\">(?P<last_update>Last updated \w+ (?P<last_update_date>\w+ \d+, \d+) at (?P<last_update_time>\d+:\d+ \w+))</p>")
-
     def __init__(self, config):
         self.config = config
 
     def get_current_month_usage(self):
         session = requests.session()
 
-        if not (self._login(session)):
-            raise RuntimeError('Login failed!')
+        usage = json.loads(session.get("https://www.videotron.com/api/1.0/internet/usage/wired/"+self.config["userkey"]+".json?lang=en&caller=videotron-mac.pommepause.com").content)
 
-        bandwidth_page = session.get("https://www.videotron.com/client/residentiel/secur/ConsommationInternet.do?locale=en")
+        update_date = datetime.datetime.strptime(usage["internetAccounts"][0]["usageTimestamp"], '%Y-%m-%dT%H:%M%z').strftime('%Y-%m-%d %H:%M')
+        current_monthly_usage = "{0:.3}".format((usage["internetAccounts"][0]["downloadedBytes"] + usage["internetAccounts"][0]["uploadedBytes"]) / (1024 * 1024 * 1024))
+        maximum_monthly_usage = (usage["internetAccounts"][0]["maxCombinedBytes"]) / (1024 * 1024 * 1024)
+        unit = 'GB'
+        days_remaining = usage["daysToEnd"]
 
-        update_date = self._get_update_date(bandwidth_page)
-        current_monthly_usage, maximum_monthly_usage, unit = self._get_bandwidth_usage(bandwidth_page)
-        days_remaining = self._get_days_remaining(bandwidth_page)
-
-        self._logout(session)
         session.close()
 
         return {
@@ -34,31 +27,3 @@ class Videotron:
             'unit': unit,
             'days_remaining': days_remaining
         }
-
-    def _login(self, session):
-        login_page = session.get("https://www.videotron.com/client/residentiel/Espace-client")
-
-        post_values = {}
-        for (input_name, input_value) in re.findall(self.PATTERN_INPUT_HIDDEN, login_page.text):
-            post_values[input_name] = input_value
-        post_values['userIdCookieEnabled'] = 'true'
-        post_values['codeUtil'] = self.config['username']
-        post_values['motDePasse'] = self.config['password']
-
-        result_login_page = session.post("https://www.videotron.com/client/user-management/residentiel/secur/Login.do", data = post_values)
-
-        return True  # TODO check if login was successful
-
-    def _get_update_date(self, bandwidth_page):
-        last_update, last_update_date, last_update_time = re.findall(self.PATTERN_LAST_UPDATE, bandwidth_page.text)[0]
-
-        return datetime.datetime.strptime(last_update_date + " " + last_update_time, '%B %d, %Y %I:%M %p').strftime('%Y-%m-%d %H:%M')
-
-    def _get_bandwidth_usage(self, bandwidth_page):
-        return re.findall(self.PATTERN_MONTHLY_INTERNET_USAGE, bandwidth_page.text)[0]  # TODO handle possible errors
-
-    def _get_days_remaining(self, bandwidth_page):
-        return re.findall(self.PATTERN_DAYS_IN_MONTH_REMAINING, bandwidth_page.text)[0]  # TODO handle possible errors
-
-    def _logout(self, session):
-        session.get("https://www.videotron.com/client/user-management/residentiel/secur/Logout.do?dispatch=logout")  # TODO check if logout was successful
